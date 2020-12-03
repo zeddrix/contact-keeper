1a07c2584a3e1371c1ac0253c9baa234ce8ddc71
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-12-03 17:09:53 +0800

    Connected app to MongoDB.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
