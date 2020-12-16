c861bc2e7660c4c022fe0fb9df2b18055d8a163f
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-12-16 16:41:42 +0800

    Able to add contact in UI.

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
@@ -12,8 +15,19 @@ const ContactForm = () => {
 
   const onChange = e => setContact({ ...contact, [e.target.name]: e.target.value })
 
+  const onSubmit = e => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal'
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className="text-primary">Add Contact</h2>
       <input
         type="text"
@@ -34,12 +48,27 @@ const ContactForm = () => {
         onChange={onChange}
         value={phone} />
       <h5>Contact Type</h5>
-      <input type="radio" name="type" value='personal' checked={type === 'personal'} />
+      <input
+        type="radio"
+        name="type"
+        value='personal'
+        checked={type === 'personal'}
+        onChange={onChange}
+      />
       Personal{' '}
-      <input type="radio" name="type" value='professional' checked={type === 'professional'} />
+      <input
+        type="radio"
+        name="type"
+        value='professional'
+        checked={type === 'professional'}
+        onChange={onChange}
+      />
       Professional{' '}
       <div>
-        <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block" />
       </div>
     </form>
   )
