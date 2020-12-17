930374ad4a7cb46410f84c8ea339aedf60458a54
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-12-17 13:52:47 +0800

    Able to filter contacts.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         type: 'professional'
       },
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -63,16 +64,27 @@ const ContactState = props => {
     dispatch({ type: UPDATE_CONTACT, payload: contact })
   };
 
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  };
+
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })
+  };
+
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}>
       {props.children}
     </ContactContext.Provider>
