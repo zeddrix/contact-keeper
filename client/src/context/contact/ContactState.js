6b3928226827ea2cf67a10389958ef0ec6d6ca35
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-12-17 10:26:24 +0800

    Able to edit and update contact.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,8 +36,9 @@ const ContactState = props => {
         phone: '0938-353-2938',
         type: 'professional'
       },
-    ]
-  }
+    ],
+    current: null
+  };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
@@ -50,12 +51,28 @@ const ContactState = props => {
     dispatch({ type: DELETE_CONTACT, payload: id })
   };
 
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact })
+  };
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT })
+  };
+
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact })
+  };
+
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
-        deleteContact
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact
       }}>
       {props.children}
     </ContactContext.Provider>
