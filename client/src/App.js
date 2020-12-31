a912d55a83b7f830a1f5811b799b16b5ff8f9ee6
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-12-31 11:29:53 +0800

    Created Register and Login forms. Cleaned up.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import './App.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import './App.css';
 
 const App = () => {
   return (
@@ -19,6 +21,9 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
