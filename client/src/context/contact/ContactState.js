e955563fbf9b2b6e5233cc325b5b7ae982a2ea6a
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-02 12:11:40 +0800

    Able to delete a contact from UI and DB.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -49,8 +49,13 @@ const ContactState = props => {
     }
   };
 
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id })
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
+    }
   };
 
   // Clear contacts array from state
