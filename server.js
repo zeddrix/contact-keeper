24fd7eb367a0c20380d2c3e0ec4eb339850920f2
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-04 15:57:03 +0800

    Few fix.

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-} else {
-
 }
 
 const PORT = process.env.PORT || 5000;
